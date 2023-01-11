3000482cb9338471aa96d7f5e1c6bd534a00ba5f
pifragile <pigu@bluewin.ch>
2023-01-11 11:19:24 +0800

    add communityIdentifier type conversion

--- src/codeGen/typeConversions.ts
+++ src/codeGen/typeConversions.ts
@@ -1,9 +1,28 @@
-const conversions = {};
+import bs58 from "bs58";
+
+const conversions = {
+    CommunityIdentifier: {
+        graphQlTypeName: "String",
+        convert: (input) => {
+            let inp = input.toHuman();
+            const geohash = inp["geohash"];
+            const digest = inp["digest"];
+            let buffer;
+            if (digest.startsWith("0x")) {
+                buffer = Buffer.from(input.toHuman()["digest"].slice(2), "hex");
+            } else {
+                buffer = Buffer.from(input.toHuman()["digest"], "utf-8");
+            }
+
+            return geohash + bs58.encode(Uint8Array.from(buffer));
+        },
+    },
+};
 
 export function getTypeConversion(encointerTypeName) {
     const { graphQlTypeName, convert } = conversions[encointerTypeName] || {
         graphQlTypeName: "String",
         convert: (input) => input.toString(),
     };
-    return { graphQlTypeName, convert }
+    return { graphQlTypeName, convert };
 }
